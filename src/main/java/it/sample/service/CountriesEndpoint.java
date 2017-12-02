20ca557848e731cdfe130cc38ec5aa36de83f9c6
Ciro <cardone.ciro@gmail.com>
2017-12-02 16:03:22 +0100

    added spring integration base configuration

---
+++ src/main/java/it/sample/service/CountriesEndpoint.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2002-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package it.sample.service;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.GenericMessage;
+import org.springframework.stereotype.Component;
+
+/**
+ * CountriesEndpoint.java: This is the default employee search service
+ *
+ * @author Vigil Bose
+ * @author Gary Russell
+ */
+@Component("countriesEndpoint")
+public class CountriesEndpoint {
+
+   private static Log log = LogFactory.getLog(CountriesEndpoint.class);
+
+   public Message<?> get(Message<?> msg) {
+
+      log.trace("Coutries Endpoint");
+      Message<String> message = new GenericMessage<>("example");
+      return message;
+   }
+
+}
+
+
